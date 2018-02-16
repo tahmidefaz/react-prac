c66b8320953f35819d5f773329b81310ec558783
Tahmid Efaz <tahmidefaz@tahmids-imac.berea.edu>
2018-02-16 02:49:43 -0500

    Used inline styling to the button

--- src/App.js
+++ src/App.js
@@ -32,11 +32,21 @@ class App extends Component {
 
   render() {
     //Call to switchNameHandler in button is inefficient, use bind as often as possible
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I am a React APP</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Adan')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Adan')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
