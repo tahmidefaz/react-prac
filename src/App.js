500221eec78c50e05defb2f5ee6afa4b56b928ea
Tahmid Efaz <tahmidefaz@tahmids-imac.berea.edu>
2018-02-17 23:21:17 -0500

    using map function to output persons

--- src/App.js
+++ src/App.js
@@ -51,19 +51,13 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
           <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}/>
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Martinez')}
-                changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}/>
-            </div>
-      )
+              {this.state.persons.map(person => {
+                return <Person
+                  name={person.name}
+                  age={person.age} />
+              })}
+          </div>
+      );
     }
 
     return (
@@ -75,6 +69,7 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
         {persons}
+
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Does this work?'));
