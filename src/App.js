42fbc7254c32f373a709c94d503db490b6e9c381
tahmidefaz <tahmidefaz@gmail.com>
2018-05-20 16:03:05 -0400

    activated css modules by ejecting configurations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -71,18 +71,18 @@ deletePersonHandler = (personIndex) => {
       style.backgroundColor = 'red';
     };
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.red);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I am a React APP</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
             style={style}
             onClick={this.togglePersonsHandler}>Toggle Persons
